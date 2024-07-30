137e4488e59e99b6ccf01d181bb461d848cee1e6
nishu-spectra <sarraf.nishu@gmail.com>
2024-07-30 15:33:42 +0545

    BFS

--- BreadthFirstSearch.java
+++ BreadthFirstSearch.java
@@ -4,29 +4,31 @@ import java.util.LinkedList;
 class BFS{
     int adjacencyMatrix[][];
     int numVertices;
+    //constructor for initializing  the graph
     BFS(int adjacencyMatrix[][])
     {
         this.adjacencyMatrix = adjacencyMatrix;
         this.numVertices = adjacencyMatrix.length;
     }
-
+   // method for performing Breadth First Search
     void BFS(int sourceVertex)
     {
         boolean visited[] = new boolean[numVertices];
-        Queue<Integer> queue = new LinkedList<Integer>();
-        queue.add(sourceVertex);
+        Queue<Integer> queue = new LinkedList<Integer>(); // initializing queue
+        queue.add(sourceVertex); // add the source vertex in the queue
         System.out.print("Breadth First Search Traversal is : ");
         while(!queue.isEmpty())
         {
-            sourceVertex = queue.poll();
+            sourceVertex = queue.poll(); //remove the fisrt element in the queue
             System.out.print(sourceVertex+" ");
-            visited[sourceVertex]= true;
+            visited[sourceVertex]= true; //mark the visited vertex as true
+            //check for the child nodes of the current visited node
             for(int i=0;i<numVertices; i++)
             {
                 if(adjacencyMatrix[sourceVertex][i]==1 && !visited[i])
                 {
-                    queue.add(i);
-                    visited[i]= true;
+                    queue.add(i); //add the child nodes in the queue
+                    visited[i]= true; //mark the added nodes as true
                 }
             }
             
